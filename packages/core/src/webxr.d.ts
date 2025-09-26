6251da4aec0c89fdbac5f056fdc0e72cf5a182f9
Jianmin Zheng <zjm@meta.com>
2025-09-26 14:34:12 -0700

    Added back missing copyright statements.

--- packages/core/src/webxr.d.ts
+++ packages/core/src/webxr.d.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // XR global augmentations centralized in @iwsdk/xr-input/src/webxr.d.ts
 
 import type { Entity } from 'elics';
--- scripts/postprocess-typedoc.cjs
+++ scripts/postprocess-typedoc.cjs
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // Post-process Typedoc Markdown to flatten the extra `src/` layer per package.
 const fs = require('fs');
 const fsp = fs.promises;
